eee2380c78bd9d9b30195cb8142e8b86cedab974
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-04-07 20:51:50 +0300

    fix: streamline chat migration process in service worker

--- src-ts/serviceWorker.ts
+++ src-ts/serviceWorker.ts
@@ -6,24 +6,28 @@ chrome.runtime.onInstalled.addListener(async ({ reason, previousVersion }) => {
   // }
 
   if (reason === "update" && previousVersion === "1.0.0") {
-    // Migrate saved chats from old storage structure to new one
-    chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
-      if (
-        changeInfo.status === "complete" &&
-        changeInfo.url &&
-        changeInfo.url.includes("chatgpt.com")
-      ) {
-        chrome.scripting.executeScript({
-          target: { tabId: tabId },
-          files: ["scripts/helpers/migrateSavedChats.js"],
-        });
-      }
-    });
+    await chrome.storage.session.set({ runMigrationOnNextChatGPT: true });
   }
 
   if (reason === "update") {
     // chrome.tabs.create({
     //   url: "whatsNewPage/whatsNew.html",
     // });
+  }
+});
+
+// Migrate saved chats from old storage structure in v1.0.0 to new one
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.url?.includes("chatgpt.com")) {
+    const { runMigrationOnNextChatGPT } = await chrome.storage.session.get(
+      "runMigrationOnNextChatGPT"
+    );
+    if (runMigrationOnNextChatGPT) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["scripts/helpers/migrateSavedChats.js"],
+      });
+      await chrome.storage.session.remove("runMigrationOnNextChatGPT");
+    }
   }
 });
