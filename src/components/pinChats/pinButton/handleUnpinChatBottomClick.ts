36e25941b2ac04a81fa10e9381eed1887d601e91
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-05-24 18:09:18 +0300

    fix: update handleUnpinChatBottomClick to use anchor element for URL ID retrieval

--- src/components/pinChats/pinButton/handleUnpinChatBottomClick.ts
+++ src/components/pinChats/pinButton/handleUnpinChatBottomClick.ts
@@ -2,16 +2,16 @@ import { removePinChatFromStorage } from "../../utils/storage";
 
 // Handle unpin chat button click
 export default async function handleUnpinChatBottomClick(
-  li: HTMLLIElement,
+  anchor: HTMLAnchorElement,
   profileId: string
 ): Promise<void> {
   try {
-    const urlId = li.querySelector("a")?.getAttribute("id");
+    const urlId = anchor.getAttribute("id");
     if (!urlId || !profileId) {
       throw new Error("URL ID or Profile ID not found");
     }
 
-    li.remove(); // Remove the pinned chat element from the DOM
+    anchor.remove(); // Remove the pinned chat element from the DOM
     await removePinChatFromStorage(profileId, urlId);
   } catch (error) {
     console.error(error);
