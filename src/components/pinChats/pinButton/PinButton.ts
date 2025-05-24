20b39fcb0b9c0545b1086d6c4ec17a3af5927d57
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-05-24 18:50:12 +0300

    fix: standardize pin and unpin button attributes for accessibility and structure

--- src/components/pinChats/pinButton/PinButton.ts
+++ src/components/pinChats/pinButton/PinButton.ts
@@ -3,6 +3,9 @@ import pinButtonElm from "./pinButton.html?raw";
 // Function to create the pin button
 export default function PinButton(): HTMLDivElement {
   const pinButton: HTMLDivElement = document.createElement("div");
+  pinButton.setAttribute("role", "menuitem");
+  pinButton.setAttribute("data-orientation", "vertical");
+  pinButton.className = "group __menu-item pe-8 gap-1.5";
   pinButton.innerHTML = pinButtonElm;
   return pinButton;
 }
--- src/components/pinChats/pinButton/UnpinButton.ts
+++ src/components/pinChats/pinButton/UnpinButton.ts
@@ -3,6 +3,9 @@ import unpinButtonElm from "./unpinButton.html?raw";
 // Function to create the unpin button
 export default function UnpinButton(): HTMLDivElement {
   const unpinButton: HTMLDivElement = document.createElement("div");
+  unpinButton.setAttribute("role", "menuitem");
+  unpinButton.setAttribute("data-orientation", "vertical");
+  unpinButton.className = "group __menu-item pe-8 gap-1.5";
   unpinButton.innerHTML = unpinButtonElm;
   return unpinButton;
 }
