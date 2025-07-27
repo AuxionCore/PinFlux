ddc538f41f92b2bebf7ba67ce09e2aae2888f003
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-07-28 00:01:50 +0300

    feat: implement saveBookmark function to handle saving bookmarks with storage capacity checks

---
+++ src/components/chatBookmarks/saveBookmark.ts
@@ -0,0 +1,49 @@
+import ensureKeysStorageCapacity from '../utils/ensureKeysStorageCapacity'
+import getCompactDateString from '../utils/getCompactDateString'
+
+export default async function saveBookmark({
+  articleId,
+  profileId,
+  conversationId,
+}: {
+  articleId: string
+  profileId: string
+  conversationId: string
+}) {
+  try {
+    const dateStr = getCompactDateString()
+    const key = `bm_${profileId}_${conversationId}_lastAccess_${dateStr}`
+    const newBookmark = { articleId: articleId }
+    const result = await browser.storage.sync.get(key)
+
+    if (key in result) {
+      const bookmarks: any[] = result[key] || []
+
+      const alreadyExists = bookmarks.some(b => b['articleId'] === articleId)
+      if (alreadyExists) {
+        return
+      }
+
+      // Check if we have enough capacity QUOTA_BYTES_PER_ITEM
+      const itemSize = new Blob([JSON.stringify(newBookmark)]).size
+      const currentSize = new Blob([JSON.stringify(bookmarks)]).size
+
+      if (currentSize + itemSize > browser.storage.sync.QUOTA_BYTES_PER_ITEM) {
+        window.alert(
+          'Not enough storage capacity to save the bookmark. Please remove some bookmarks first.'
+        )
+
+        console.warn('Not enough storage capacity to save the bookmark')
+        return
+      }
+
+      const updatedBookmarks = [...bookmarks, newBookmark]
+      await browser.storage.sync.set({ [key]: updatedBookmarks })
+    } else {
+      await ensureKeysStorageCapacity()
+      await browser.storage.sync.set({ [key]: [newBookmark] })
+    }
+  } catch (error) {
+    console.error('Failed to save bookmark:', error)
+  }
+}
