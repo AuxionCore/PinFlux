da7ba6921a99d048b8f475d8cf34dd821c483b05
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-08-01 00:34:37 +0300

    refactor: remove console logs for cleaner code in replaceBookmarkButton function

--- src/components/chatBookmarks/replaceBookmarkButton.ts
+++ src/components/chatBookmarks/replaceBookmarkButton.ts
@@ -1,25 +1,16 @@
 export default function replaceBookmarkButton(section: Element, newHtml: string) {
-  console.log('replaceBookmarkButton called with:', section, newHtml)
   // החיפוש נעשה ב-container (האלמנט ההורה)
   const container = section.parentElement
-  if (!container) {
-    console.log('No container found')
-    return
-  }
+  if (!container) return
   
   const oldButton = container.querySelector('[data-bookmark-button]')
-  if (oldButton) {
-    console.log('Removing old button:', oldButton)
-    oldButton.remove()
-  }
+  if (oldButton) oldButton.remove()
   
-  console.log('Adding new button to container:', container)
   container.insertAdjacentHTML('beforeend', newHtml)
   
   // צריך להוסיף את הattributes לכפתור החדש
   const newButton = container.querySelector('[data-bookmark-button]:last-child') as HTMLElement
   if (newButton && section.id) {
     newButton.setAttribute('data-section-id', section.id)
-    console.log('Added data-section-id to new button:', section.id)
   }
 }
